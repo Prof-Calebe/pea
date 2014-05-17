80e9375f8b6d8676665997be30d28e6a71c42426
Adriano <Adriano@ADRIANO-PC>
2014-05-17 10:13:54 -0300

    - Inserted some basic assert check to ensure the database connection is open; - Removed old unused methods;

--- Pea1/src/java/Classes/BaseDAO.java
+++ Pea1/src/java/Classes/BaseDAO.java
@@ -4,13 +4,6 @@
  */
 package Classes;
 
-/**
- *
- * @author Song
- */
-
-import java.util.HashMap;
-import java.util.Map;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -18,46 +11,48 @@ import javax.persistence.Query;
 
 
 public class BaseDAO {
-	//fabrica de conexao
-	private EntityManagerFactory factory;
-	//manager de objetos do banco
-	private EntityManager manager;
+    public BaseDAO ()
+    {
+        isOpen = false;
+    }
 	
 	//devolve o manager em uso
 	public EntityManager retornaManager(){
+        assert(isOpen):"Database is not yet opened.";
 		return manager;
 	}
 	
 	public EntityManagerFactory retornaFactory(){
-		
+		assert(isOpen):"Database is not yet opened.";
 		return factory;
 	}
 	
 	
 	public void abreDB(){
-		factory = Persistence.createEntityManagerFactory("$objectdb/db/Pea.odb"); 
+		factory = Persistence.createEntityManagerFactory("$objectdb/db/Pea.odb");
 		manager = factory.createEntityManager();
+        isOpen = true;
 	}
 	
 	public void fechaDB(){
-		
 		manager.close();
 		factory.close();
+        isOpen = false;
 	}
 	
 	public Object retornaAno(){
-		
+        assert(isOpen):"Database is not yet opened.";
 		Query minhaQuery = retornaManager().createNativeQuery("select year(curdate())");
 		return minhaQuery.getSingleResult();
 	}
 	
 	//salvar como pra atualizar os dados
 	public void salvar(Object o){
-        System.out.println("asd");
+        assert(isOpen):"Database is not yet opened.";
 		try{
-        manager.getTransaction().begin();
-        manager.persist(o);
-        manager.getTransaction().commit();
+            manager.getTransaction().begin();
+            manager.persist(o);
+            manager.getTransaction().commit();
 		}catch(Exception e){
 			System.out.println("deu erro de salvar");
 		}
@@ -65,19 +60,13 @@ public class BaseDAO {
 	
 	//remove 
 	public void remover(Object o){
+        assert(isOpen):"Database is not yet opened.";
 		manager.getTransaction().begin();
         manager.remove(o);
 		manager.getTransaction().commit();
 	}
-	
-	public void iguala(BaseDAO b)
-	{
-	this.factory = b.retornaFactory();
-	this.manager = b.retornaManager();
-	}
-	
-	public BaseDAO getDAO()
-	{
-		return this;
-	}
+    
+    private boolean isOpen;
+	private EntityManagerFactory factory;
+	private EntityManager manager;
 }
