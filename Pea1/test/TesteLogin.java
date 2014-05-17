7eaf930ed173944ca4827216386cacdaaa72d730
Adriano <Adriano@ADRIANO-PC>
2014-05-17 16:29:00 -0300

    - Updated tests, now it has 100% code coverage; - Inserted Login validation tests; - Inserted Login loading tests;

--- Pea1/test/TesteLogin.java
+++ Pea1/test/TesteLogin.java
@@ -3,24 +3,21 @@
  * and open the template in the editor.
  */
 
+import Classes.BaseDAO;
 import Classes.Login;
 import junit.framework.Assert;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
  * @author Song
  */
 public class TesteLogin {
-    
-    public TesteLogin() {
-    }
+    private BaseDAO dataBaseConnection;
     
     @BeforeClass
     public static void setUpClass() {
@@ -32,21 +29,82 @@ public class TesteLogin {
     
     @Before
     public void setUp() {
+        dataBaseConnection = new BaseDAO();
+        dataBaseConnection.abreDB();
     }
-    
+
     @After
     public void tearDown() {
+        dataBaseConnection.fechaDB();
     }
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+
     @Test
-    public void TestDataIntegrity(){
+    public void TestDataIntegrity()
+    {
         Login credentials = new Login("Nome", "Senha", "Acesso");
+        
         Assert.assertEquals("Nome", credentials.getNome());
         Assert.assertEquals("Senha", credentials.getSenha());
         Assert.assertEquals("Acesso", credentials.getTipoLogin());
+        
+        credentials.setNome("NomeEstranho");
+        Assert.assertEquals("NomeEstranho", credentials.getNome());
+        credentials.setSenha("SenhaEstranha");
+        Assert.assertEquals("SenhaEstranha", credentials.getSenha());
+    }
+    
+    @Test
+    public void TestValidLogin()
+    {
+        Login credentials = new Login("usuarioTeste123", "senhaTeste123", "Acesso");
+        dataBaseConnection.salvar(credentials);
+        
+        Login credentials2 = new Login("usuarioTeste123", "senhaTeste123", "");
+        credentials2.Validate();
+        Assert.assertTrue(credentials2.isValid());
+        Assert.assertEquals(credentials.getId(), credentials2.getId());
+        
+        // Cleanup
+        dataBaseConnection.retornaManager().clear();
+        credentials = dataBaseConnection.retornaManager().find(Login.class, credentials.getId());
+        dataBaseConnection.remover(credentials);
+    }
+    
+    @Test
+    public void TestInvalidLogin()
+    {
+        Login credentials = new Login("usuarioTeste123", "senhaTeste123", "Acesso");
+        dataBaseConnection.salvar(credentials);
+        
+        Login credentials2 = new Login("usuarioTeste123", "senhateste123", "Acesso");
+        credentials2.Validate();
+        Assert.assertFalse(credentials2.isValid());
+        
+        // Cleanup
+        dataBaseConnection.retornaManager().clear();
+        credentials = dataBaseConnection.retornaManager().find(Login.class, credentials.getId());
+        dataBaseConnection.remover(credentials);
+    }
+    
+    @Test
+    public void TestValidIDLoading()
+    {
+        Login credentials = new Login("usuarioTeste123", "senhaTeste123", "Acesso");
+        dataBaseConnection.salvar(credentials);
+        
+        Login credentials2 = Login.LoadLoginByID(credentials.getId());
+        Assert.assertEquals(credentials.getId(), credentials2.getId());
+        
+        // Cleanup
+        dataBaseConnection.retornaManager().clear();
+        credentials = dataBaseConnection.retornaManager().find(Login.class, credentials.getId());
+        dataBaseConnection.remover(credentials);
+    }
+    
+    @Test
+    public void TestInvalidIDLoading()
+    {
+        Login credentials2 = Login.LoadLoginByID(-1);
+        Assert.assertNull(credentials2);
     }
 }
