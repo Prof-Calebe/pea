3b0a04f7d95cec30d7aa4615106ec436b3e6fd45
Arturo Sekijima <arturoseki@yahoo.es>
2014-05-18 20:44:22 -0300

    Adicionado Teste para Validar autorização de professor

--- Pea1/test/TesteAutorizarProfessor.java
+++ Pea1/test/TesteAutorizarProfessor.java
@@ -5,12 +5,15 @@
  */
 
 import Classes.BaseDAO;
+import Classes.Colegiado;
+import Classes.NDE;
+import Classes.Professor;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
@@ -39,6 +42,37 @@ public class TesteAutorizarProfessor {
     public void tearDown() {
         dataBaseConnection.fechaDB();
     }
+    
+    @Test
+    public void ValidaAutoriza(){
+        Professor p1 = new Professor(12345, "Calebe");
+        Professor p2 = new Professor(12346, "Joaquim");
+        Colegiado p3 = new Colegiado(123, "Ana");
+        NDE p4 = new NDE(12, "Vallim");
+
+        dataBaseConnection.salvar(p1);
+        dataBaseConnection.salvar(p2);
+        dataBaseConnection.salvar(p3);
+        dataBaseConnection.salvar(p4);
+        
+        Professor p5 = new Professor(12345, "Calebe");
+        Professor p6 = new Professor(12346, "Joaquim");
+        Colegiado p7 = new Colegiado(123, "Ana");
+        NDE p8 = new NDE(12, "Vallim");
+        
+        Assert.assertTrue (p8.Validate(p5));
+        Assert.assertTrue (p8.Validate(p6));
+        Assert.assertTrue (p8.Validate(p7));
+        
+        Assert.assertTrue(p8.AutorizarProfessor(12345,"Calebe"));
+        Assert.assertTrue(p8.AutorizarProfessor(12346,"Joaquim"));
+        
+        
+    }
+    
+    
+    
+    
 
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
